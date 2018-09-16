abcf52e152718f101434f505399bb67306c1b3e0
Abigayle Franklin <abifanklin@gmail.com>
2018-09-16 10:41:28 -0500

    Volunteer page done

---
+++ page-volunteer.php
@@ -0,0 +1,24 @@
+<?php
+
+get_header(); ?>
+<div class="hero-area-volunteer">
+    <h1><?php bloginfo('description') ?></h1>
+</div>
+
+    <?php
+    $query = new WP_Query( array( 'category_name' => 'volunteer' ) );
+    if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post(); ?>
+        <article <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        </article>
+       <?php endwhile;
+    else :
+        echo 'No posts';
+    endif;
+
+
+get_footer();
+
+?>
