a195f77f5d177271548d5de4c0e369d29d5a00f3
Abigayle Franklin <abifanklin@gmail.com>
2018-09-16 09:45:12 -0500

    Added conditionals for filtering posts.

--- index.php
+++ index.php
@@ -1,28 +1,46 @@
 <?php
 
-
 get_header(); ?>
 <div class="hero-area">
     <h1><?php bloginfo('description') ?></h1>
 </div>
-<h2 class="featured-pets">Featured Pets</h2>
 <?php
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-    
-
-    <article <?php post_class(); ?>>
+    $query = new WP_Query( array( 'category_name' => 'urgent' ) );
+    if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post(); ?>
+        <article <?php post_class(); ?>>
         <h2><?php the_title(); ?></h2>
         <p><?php the_content(); ?></p>
-    </article>
-
-
-    <?php endwhile;
-
+        </article>
+       <?php endwhile;
+    endif; ?>
+<h2 class="featured-pets">Featured Pets</h2>
+<?php
+    $query = new WP_Query( array( 'category_name' => 'featured' ) );
+    if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post(); ?>
+        <article <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        </article>
+       <?php endwhile;
     else :
-        echo '<p>No content found</p>';
+        echo 'No posts';
+    endif; ?>
 
+    <?php
+    $query = new WP_Query( array( 'category_name' => 'about' ) );
+    if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post(); ?>
+        <article <?php post_class(); ?>>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+        </article>
+       <?php endwhile;
+    else :
+        echo 'No posts';
     endif;
+
 
 get_footer();
 
