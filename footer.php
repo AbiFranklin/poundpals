32309794a013c585dcebbfb9518d229b413bad84
Kristen Jaimes <meek.kristen@gmail.com>
2018-09-15 19:17:54 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!-- FOOTER -->
+    <footer id="footer">
+        <p class="center">Calhoun County Humane Society &bull; 201 Stringham Drive Port Lavaca, TX 77979 &bull; 361-553-8916</p>
+    </footer>
+    <?php wp_footer(); ?>
+
+	</main>
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+	 crossorigin="anonymous"></script>
+	<script src="js/bootstrap.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+
+<header>
+	<nav class="navbar navbar-expand-md fixed-top">
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Adopt</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Volunteer</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Give</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#about">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#footer">Contact</a>
+				</li>
+			</ul>
+			<h1>Calhoun County Humane Society</a>
+		</div>
+	</nav>
+</header>
